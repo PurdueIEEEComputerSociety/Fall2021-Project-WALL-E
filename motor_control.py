635def4cc6008ae80db0fb9e1d5ffb6b814e8240
Adampi210 <adampi210u@gmail.com>
2021-11-18 20:23:28 -0500

    fixed functions

--- motor_control.py
+++ motor_control.py
@@ -28,13 +28,14 @@ class MotorController():
             time.sleep(0.001)
         GPIO.output(self.left_motor_pin, GPIO.LOW)
 
-    def _go_straight(self, desired_time, speed)
+    def _go_straight(self, desired_time, speed):
         tme = 0
         while tme <= desired_time:
             GPIO.PWM(self.left_motor_pin, speed)
             GPIO.PWM(self.right_motor_pin, speed)
 
             tme += 0.001
+            print(f'going straight for: {tme}.')
             time.sleep(0.001)
         GPIO.output(self.left_motor_pin, GPIO.LOW)
         GPIO.output(self.right_motor_pin, GPIO.LOW)
@@ -44,4 +45,4 @@ if __name__ == '__main__':
     channel2 = 2
     motorController = MotorController(channel1, channel2)
     motorController.GPIO_setup()
-    motorController._turn_left_time(1)
+    motorController._go_straight(2, 0.8)
